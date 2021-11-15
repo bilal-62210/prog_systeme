a3bea1a532cc645022a08da9901dc3b356f457ec
bilal-62210 <bilal.benelkadi@viacesi.fr>
2021-11-15 14:54:49 +0100

    ok

--- prog_systeme_form/fichiers d'en tête/Acceuil.Designer.cs
+++ prog_systeme_form/fichiers d'en tête/Acceuil.Designer.cs
@@ -7,6 +7,7 @@ namespace prog_systeme_form.fichiers_d_en_tête
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -29,12 +30,32 @@ namespace prog_systeme_form.fichiers_d_en_tête
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(336, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Acceuil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Acceuil";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        
     }
 }
