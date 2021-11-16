68b5363163df08bb76c3d839ebc3b1de7b0e6d94
bilal-62210 <bilal.benelkadi@viacesi.fr>
2021-11-16 10:00:52 +0100

    ok

--- appli_console/appli_console/Program.cs
+++ appli_console/appli_console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO
 
 namespace appli_console
 {
@@ -6,7 +7,64 @@ namespace appli_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var etat = true;
+
+            while (etat == true)
+            {
+                // Ask the user to choose an option.
+                Console.WriteLine("Choose an option from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\tm - Modify");
+                Console.WriteLine("\td - Delete");
+                Console.WriteLine("\tr - Read");
+                Console.WriteLine("\tq - Quit");
+                Console.Write("Your option : ");
+
+                // Use a switch statement to do the math.
+                switch (Console.ReadLine())
+                {
+                    case "a":
+                        var result = new sauvegarde();
+
+                        Console.Write("Name of the save : ");
+                        var NameSave = Console.ReadLine();
+                        Console.Write("Source of the save : ");
+                        var SourceSave = Console.ReadLine();
+                        Console.Write("Cible of the save : ");
+                        var TargetSave = Console.ReadLine();
+                        Console.Write("Type of the save : ");
+                        var TypeSave = Console.ReadLine();
+
+                        Console.Write(NameSave + ' ' + SourceSave + ' ' + TargetSave + ' ' + TypeSave);
+                        //Pass the filepath and filename to the StreamWriter Constructor
+                        StreamWriter sw = new StreamWriter("");
+                        //Write a line of text
+                        sw.WriteLine("Hello World!!");
+                        //Write a second line of text
+                        sw.WriteLine("From the StreamWriter class");
+                        //Close the file
+                        sw.Close();
+                        // result.Create(NameSave, SourceSave, TargetSave, TypeSave);
+
+                        break;
+                    case "m":
+
+                        break;
+                    case "d":
+
+                        break;
+                    case "r":
+                        var liste = new sauvegarde();
+                        liste.List();
+                        break;
+                    case "q":
+                        etat = false;
+                        break;
+                }
+            }
+            // Wait for the user to respond before closing.
+            Console.Write("\n Press any key to close the Calculator console app...");
+            Console.ReadKey();
         }
     }
 }
---
+++ appli_console/appli_console/sauvegarde.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appli_console
+{
+    class sauvegarde
+    {
+        protected string Name;
+        protected string Source;
+        protected string Target;
+        protected string Type;
+        protected Boolean Choix;
+        protected int Temps;
+        protected int Nb = 0;
+        protected List<string> ListSave = new List<string>();
+
+        public void List()
+        {
+            for (int i = 0; i < ListSave.Count; i++)
+            {
+                Console.WriteLine(ListSave[i]);
+            }
+        }
+
+        public void Create(string NameSave, string SourceSave, string TargetSave, string TypeSave)
+        {
+            Nb += 1;
+            if (Nb < 5)
+            {
+                ListSave.Add(NameSave);
+                ListSave.Add(SourceSave);
+                ListSave.Add(TargetSave);
+                ListSave.Add(TypeSave);
+                ListSave.Add("n");
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+        private void Modify(string NameSave, string SourceSave, string TargetSave, string TypeSave)
+        {
+
+        }
+        private void Delete(string NameSave)
+        {
+
+        }
+        private void Save(string NameSave)
+        {
+
+        }
+        private void SequentialSave(string NameSave, Boolean ChoixSequentail, int TempsSequential)
+        {
+
+        }
+    }
+}
