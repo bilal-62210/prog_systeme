ac5ea4b5fb043997292374e1f9853af5f5d70cf4
Thomas DUCHATEAU <thomas.duchateau1@viacesi.fr>
2021-12-08 17:36:32 +0100

    Create branch for deploy V1.0

--- EasySaveV1.0/obj/Debug/netcoreapp3.1/EasySaveV1.0.AssemblyInfo.cs
+++ EasySaveV1.0/obj/Debug/netcoreapp3.1/EasySaveV1.0.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("appli_console")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EasySaveV1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("appli_console")]
-[assembly: System.Reflection.AssemblyTitleAttribute("appli_console")]
+[assembly: System.Reflection.AssemblyProductAttribute("EasySaveV1.0")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EasySaveV1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
